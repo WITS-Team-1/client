8721f419f4d56baff2151ed1e2a318bd92d19f3d
asheik55 <asheik55@uwo.ca>
2022-01-24 19:21:27 -0500

    Splash Page
    I added the Splash Page, I exported the logo off Figma and put it in.

---
+++ src/SplashScreen/SplashScreen.js
@@ -0,0 +1,16 @@
+import React from "react"
+import logo from "./Group 68.png"
+
+
+const SplashScreen = () => {
+    return (
+        <div>
+
+          <img src={logo}/>
+        </div>
+    );
+};
+
+
+
+export default SplashScreen;
