eabbb3d33b3a71dcce6f178084389bb22e6ce5ba
abalteanu <58431085+abalteanu@users.noreply.github.com>
2022-02-04 16:55:43 -0500

    Basic Carousel html components w/o carousel styling
    need to add carousel styling

--- src/ChooseThemeComponent/ChooseTheme.js
+++ src/ChooseThemeComponent/ChooseTheme.js
@@ -1,9 +1,10 @@
 import './ChooseTheme.css';
 import ThemeOption from './ThemeOption';
 import Rome from "./assets/Rome.jpg";
+import Tokyo from "./assets/Tokyo.jpg";
 import ArrowL from"./assets/ArrowL.png";
 import ArrowR from"./assets/ArrowR.png";
-import Carousel from "react-elastic-carousel";
+import Carousel from 'react-elastic-carousel';
 import React, { Component } from 'react';
 
 // import Item from "./ThemeOption";
@@ -15,21 +16,34 @@ import React, { Component } from 'react';
 // how do i center the main container on the screen 
 
 class ChooseTheme extends Component{
-    state = {
-        items: [
-            Rome,
-            Rome
-        ]
-      }
-    
-      render () {
-        const { items } = this.state;
-        return (
-          <Carousel>
-            {items.map(item => <div key={item.id}>{item.url}</div>)}
-          </Carousel>
-        )
-      }
+  state = {
+      items: [
+        {
+          imgSource: Rome,
+          name: 'ROME',
+        },
+        {
+          imgSource: Tokyo,
+          name: 'TOKYO',
+        },    
+      ],
+    };
+
+  render () {
+    const { items } = this.state;
+    return (
+      <Carousel>
+        {items.map((item, index) => (
+          <div className='carousel-item-container' key={index}>
+            <img src={item.imgSource} className='option' alt={item.name}/>
+            <button className='option-btn'>
+              <p>{item.name}</p>
+            </button>
+          </div>
+        ))}
+      </Carousel>
+    );
+  }
 }
 
 export default ChooseTheme;
