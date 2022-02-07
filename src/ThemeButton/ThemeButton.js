70c52884b415fc6589451e1d1581926a10caac5a
Mylan Nguyen <mnguy246@uwo.ca>
2022-02-07 10:49:47 -0500

    Version 2.0
    Styling fix.

--- src/ThemeButton/ThemeButton.js
+++ src/ThemeButton/ThemeButton.js
@@ -4,12 +4,12 @@ function ThemeButton() {
     return (
         <div id="container">
         <div id="background">
-        <h1 id="selectThemeTitle">SELECT YOUR THEME</h1>
-        <button class="themeButtons">CAFE</button>
-        <button class="themeButtons">CITY SCAPES</button>
-        <button class="themeButtons">LANDSCAPES</button>
-        <button class="themeButtons">ABSTRACT</button>
-        <button class="themeButtons">SOLID COLOURS</button>
+            <h1 id="selectThemeTitle">SELECT YOUR THEME</h1>
+            <button class="themeButtons">CAFE</button>
+            <button class="themeButtons">CITY SCAPES</button>
+            <button class="themeButtons">LANDSCAPES</button>
+            <button class="themeButtons">ABSTRACT</button>
+            <button class="themeButtons">SOLID COLOURS</button>
         </div>
     </div>
     
