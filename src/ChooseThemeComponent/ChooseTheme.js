2ef207347d369ff63dbf157da329e445c72c9405
abalteanu <58431085+abalteanu@users.noreply.github.com>
2022-02-07 15:53:56 -0500

    Finished formatting
    Must add basic functionality to buttons (so that functions can be easily added later)
    
    - Added formatting to the dots at the bottom (instead of being purple, they are now the greys from figma)
    - Made the entire carousel dynamic, so that it can change the number of items shown in the window based on the window's size (had to add padding to the .App formatting in App.css for this to look good)
    - Added the arrows (images from figma that are pressable)
    - Added font
    
    What I can do to make this better:
    - Add more background options (more than four so there are more to scroll through
    - Make it so that when you reach the last item, it returns to the first item if you press the arrow again (this has proven to be difficult, I tried to find a solution but that kind of looping is not very compatible with react-elastic-carousel, so the solution was very long)
    
    Helpful resources I used: https://sag1v.github.io/react-elastic-carousel/renderPagination
    https://github.com/sag1v/react-elastic-carousel/tree/master/src/docs/components

--- src/ChooseThemeComponent/ChooseTheme.js
+++ src/ChooseThemeComponent/ChooseTheme.js
@@ -2,18 +2,20 @@ import './ChooseTheme.css';
 import ThemeOption from './ThemeOption';
 import Rome from "./assets/Rome.jpg";
 import Tokyo from "./assets/Tokyo.jpg";
+import NewYork from "./assets/NewYork.jpg";
+import Hawaii from "./assets/Hawaii.jpg"
 import ArrowL from"./assets/ArrowL.png";
 import ArrowR from"./assets/ArrowR.png";
-import Carousel from 'react-elastic-carousel';
+import Carousel, {consts} from 'react-elastic-carousel';
+import Flex from './Flex.js';
+import Dot from './Dot.js';
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 
 // import Item from "./ThemeOption";
 
 
-//it doesnt like it when i use body tag
-//how to implement font
-// do i have to import every image
-// how do i center the main container on the screen 
 
 class ChooseTheme extends Component{
   state = {
@@ -25,23 +27,91 @@ class ChooseTheme extends Component{
         {
           imgSource: Tokyo,
           name: 'TOKYO',
-        },    
+        },
+        {
+          imgSource: NewYork,
+          name: 'NEW YORK',
+        },
+        {
+          imgSource: Hawaii,
+          name: 'HAWAII',
+        },
       ],
     };
+
+    
+  myArrow({ type, onClick, isEdge }) {
+    const pointer = type === consts.PREV ? <img src={ArrowL} /> : <img src={ArrowR} />
+
+    const Button = styled.button`
+      background-color: Transparent;
+      border-radius: 20px;
+      background-repeat:no-repeat;
+      border: none;
+      cursor:pointer;
+    `
+
+    return (
+      <Button onClick={onClick} disabled={isEdge}>
+        {pointer}
+      </Button>
+    )
+  }
+
+  constructor(props) {
+    super(props)
+    this.breakPoints = [
+      { width: 1, itemsToShow: 1 },
+      { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
+      { width: 850, itemsToShow: 3 },
+      { width: 1150, itemsToShow: 4, itemsToScroll: 2 },
+      // { width: 1450, itemsToShow: 5 },
+      // { width: 1750, itemsToShow: 6 },
+    ]
+  }
 
   render () {
     const { items } = this.state;
     return (
-      <Carousel>
-        {items.map((item, index) => (
-          <div className='carousel-item-container' key={index}>
-            <img src={item.imgSource} className='option' alt={item.name}/>
-            <button className='option-btn'>
-              <p>{item.name}</p>
-            </button>
-          </div>
-        ))}
-      </Carousel>
+      <div class="main-container">
+        <Carousel 
+          breakPoints={this.breakPoints}
+          renderArrow={this.myArrow}
+          itemsToScroll={1}
+          itemsToShow={4}
+          tiltEasing="cubic-bezier(0.110, 1, 1.000, 0.210)"
+          transitionMs={700}
+
+          renderPagination={({ pages, activePage, onClick }) => {
+            return (
+              <Flex direction="row">
+                {pages.map(page => {
+                  const isActivePage = activePage === page
+                  return (
+                    <Dot
+                    key={page}
+                    onClick={() => onClick(page)}
+                    active={isActivePage}
+                    />
+                    
+                  )
+                })}
+              </Flex>
+            )
+          }}
+          >
+
+          {items.map((item, index) => (
+            <div className='carousel-item-container' key={index}>
+              <img src={item.imgSource} className='option' alt={item.name}/>
+              <button className='option-btn'>
+                <p>{item.name}</p>
+              </button>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+
     );
   }
 }
