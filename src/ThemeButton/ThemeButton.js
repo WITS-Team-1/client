3103061d84f008b635a70510af7b2b75cc36c907
Mylan Nguyen <mnguy246@uwo.ca>
2022-02-06 22:28:14 -0500

    Version 1.2
    Got all the components in. Just need to fix the sizing and padding for the screen.

--- src/ThemeButton/ThemeButton.js
+++ src/ThemeButton/ThemeButton.js
@@ -2,8 +2,17 @@ import './ThemeButton.css';
 
 function ThemeButton() {
     return (
-        <div className="main-container">
+        <div id="container">
+        <div id="background">
+        <h1 id="selectThemeTitle">SELECT YOUR THEME</h1>
+        <button class="themeButtons">CAFE</button>
+        <button class="themeButtons">CITY SCAPES</button>
+        <button class="themeButtons">LANDSCAPES</button>
+        <button class="themeButtons">ABSTRACT</button>
+        <button class="themeButtons">SOLID COLOURS</button>
+        </div>
     </div>
+    
     )
 }
 export default ThemeButton;
