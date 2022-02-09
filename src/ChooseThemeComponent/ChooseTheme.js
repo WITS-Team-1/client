a8d4be13111c34dc7a64e995c069f24d7994ddce
abalteanu <58431085+abalteanu@users.noreply.github.com>
2022-02-08 23:52:13 -0500

    Removed comments

--- src/ChooseThemeComponent/ChooseTheme.js
+++ src/ChooseThemeComponent/ChooseTheme.js
@@ -60,8 +60,6 @@ class ChooseTheme extends Component{
       { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
       { width: 850, itemsToShow: 3 },
       { width: 1150, itemsToShow: 4, itemsToScroll: 2 },
-      // { width: 1450, itemsToShow: 5 },
-      // { width: 1750, itemsToShow: 6 },
     ]
   }
 
