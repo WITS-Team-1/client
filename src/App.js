dc4c88665017e277561f6ab9463f692c2b11228d
Ana Balteanu <58431085+abalteanu@users.noreply.github.com>
2022-02-09 20:04:16 -0500

    Choose theme component (#17)
    * General format
    
    I figured out the general format of how this component will look, and divided it up into containers.
    I have yet to add functionality to the buttons
    
    * Carousel basics
    
    All old work is in ThemeOption.js
    need to figure out how to add images to the carousel
    
    * Basic Carousel html components w/o carousel styling
    
    need to add carousel styling
    
    * Finished formatting
    
    Must add basic functionality to buttons (so that functions can be easily added later)
    
    - Added formatting to the dots at the bottom (instead of being purple, they are now the greys from figma)
    - Made the entire carousel dynamic, so that it can change the number of items shown in the window based on the window's size (had to add padding to the .App formatting in App.css for this to look good)
    - Added the arrows (images from figma that are pressable)
    - Added font
    
    What I can do to make this better:
    - Add more background options (more than four so there are more to scroll through
    - Make it so that when you reach the last item, it returns to the first item if you press the arrow again (this has proven to be difficult, I tried to find a solution but that kind of looping is not very compatible with react-elastic-carousel, so the solution was very long)
    
    Helpful resources I used: https://sag1v.github.io/react-elastic-carousel/renderPagination
    https://github.com/sag1v/react-elastic-carousel/tree/master/src/docs/components
    
    * Made Edits (comments + formatting)
    
    deleted unnecessary comments and fixed react formatting
    
    Not sure where to place the carousel itself on the website. I resolved the merge issue but right now i just have the carousel on one side of the screen, which is probably not what we want for the actual website.
    
    * Moved images into general assets folder
    
    * Removed comments
    
    Co-authored-by: Ewhan Han <ewhan.han@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import ClockComponent from './ClockComponent/ClockComponent';
 import DateComponent from './ClockComponent/DateComponent';
+import ChooseTheme from './ChooseThemeComponent/ChooseTheme';
 
 import SplashScreen from './SplashScreen/SplashScreen';
 
 function App() {
   return (
     <div className='App'>
+      <ChooseTheme /> 
       <div className='font-face'>
         <header className='App-header'>
+
           <ClockComponent />
           <DateComponent />
           <SplashScreen />
