6ace86680f4479d1b6e91ded38913469183de0d4
Ewhan Han <ewhan.han@gmail.com>
2022-03-05 16:21:52 -0500

    Fix(cleanup): font and webvitals

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './fonts/Korolev-Medium.woff';
-import './fonts/Korolev-Bold.woff';
-import './fonts/Korolev-Heavy.woff';
+import './assets/fonts/Korolev-Medium.woff';
+import './assets/fonts/Korolev-Bold.woff';
+import './assets/fonts/Korolev-Heavy.woff';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
@@ -23,8 +22,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
