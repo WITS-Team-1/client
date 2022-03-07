62ee4fe966f70b15799444e4c09e4bda90e213cb
rachzhu <rachelzhuu@gmail.com>
2022-03-07 18:15:45 -0500

    add ewhan's version

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-
+//import './App.css';
 
 import ClockComponent from './ClockComponent/ClockComponent';
 import DateComponent from './ClockComponent/DateComponent';
 import ChooseTheme from './ChooseThemeComponent/ChooseTheme';
 import SoundWidget from './SoundWidget/SoundWidget';
 import SplashScreen from './SplashScreen/SplashScreen';
+import SoundWidgetv2 from './SoundWidget2/SoundWidget';
 
 function App() {
   return (
-    <div className='App'>
-      <ChooseTheme /> 
-      <div className='font-face'>
-        <header className='App-header'>
-          <ClockComponent />
-          <DateComponent />
-          <SplashScreen />
-        </header>
-        <SoundWidget/>
-      </div>
-    </div>
+    // <div className='App'>
+    //   <ChooseTheme /> 
+    //   <div className='font-face'>
+    //     <header className='App-header'>
+    //       <ClockComponent />
+    //       <DateComponent />
+    //       <SplashScreen />
+    //     </header>
+    //     <SoundWidget/>
+    //   </div>
+    // </div>
+    <SoundWidgetv2/>
   );
 }
 
