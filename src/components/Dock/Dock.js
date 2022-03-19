d63e4b1e5c7fbab4fba38e844736544f37ecc2b1
Joud <66938933+joudlahham@users.noreply.github.com>
2022-03-19 10:56:38 -0400

    Calender widget (#25)
    * Issues connecting the Calendar logo to calendar
    
    * trying to alter the css
    
    * Used calendar library, trigger not working
    
    * added modules back
    
    * added functionality of calender button on dock
    
    * Calendar icon opens calendar, exit button closes it. Need assitance with css on exit button & calendar icon (no focus).
    
    * Fixed Calendar widget

--- src/components/Dock/Dock.js
+++ src/components/Dock/Dock.js
@@ -1,54 +1,90 @@
-import React from 'react';
-import './Dock.css';
-import themes from '../../assets/images/themes.png';
-import calendar from '../../assets/images/calendar.png';
-import timer from '../../assets/images/timer.png';
-import spotify from '../../assets/images/spotify.png';
-import todolist from '../../assets/images/todolist.png';
-import sounds from '../../assets/images/sounds.png';
-import stickers from '../../assets/images/stickers.png';
-import { Link } from 'react-router-dom';
+    import React from 'react';
+    import './Dock.css';
+    import themes from '../../assets/images/dock/themes.png';
+    import calendar from '../../assets/images/dock/calendar.png';
+    import timer from '../../assets/images/dock/timer.png';
+    import spotify from '../../assets/images/dock/spotify.png';
+    import todolist from '../../assets/images/dock/todolist.png';
+    import sounds from '../../assets/images/dock/sounds.png';
+    import stickers from '../../assets/images/dock/stickers.png';
+    import DatePicker from 'sassy-datepicker';
+    import exit from "../../assets/images/x_button.png"
 
-const Dock = (props) => {
-  const pomodoroHandler = (e) => {
-    e.preventDefault();
-    props.showPomodoro();
-  };
+    class Dock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+        };
+        this.toggleShowHide = this.toggleShowHide.bind(this)
+        this.Calendar = this.Calendar.bind(this)
+        this.openCalendar = this.openCalendar.bind(this)
+    }
+      
+    Calendar =() => {
+        const onChange = (date) => {
+        <button aria-label='edit'>
+        <edit-icon />
+      </button>
+      };
+    }
+    openCalendar = () => {
+        this.setState({isOpen: true});
+    };
+    toggleShowHide = () => {       
+        this.setState(state => ({ isOpen: !state.isOpen }));
+      };
 
-  return (
-    <React.Fragment>
-      <div className='footer'>
-        <div id='container'>
-          <div id='cont'>
-            <div className='footer_center'>
-              <section id='icons'>
-                <Link to='/chooseTheme'>
-                  <img src={themes} width='60' height='60' alt='Theme' />
-                </Link>
-                <a href='/index.html'>
-                  <img src={calendar} width='60' height='60' alt='Calendar' />
-                </a>
-                <button onClick={pomodoroHandler}>
-                  <img src={timer} width='60' height='60' alt='Timer' />
-                </button>
-                <a href='index.html'>
-                  <img src={spotify} width='60' height='60' alt='Spotify' />
-                </a>
-                <a href='index.html'>
-                  <img src={todolist} width='60' height='60' alt='To-do List' />
-                </a>
-                <a href='index.html'>
-                  <img src={sounds} width='60' height='60' alt='Sounds' />
-                </a>
-                <a href='index.html'>
-                  <img src={stickers} width='60' height='60' alt='Sticky' />
-                </a>
-              </section>
+    render() {
+        return (
+        <React.Fragment>
+            <div className='calendarwidget'>
+                {this.state.isOpen?   
+                <React.Fragment>
+                    <div className='calendarFonts'>
+                <button className='button-exit'><img src={exit} alt="exit" onClick={this.toggleShowHide}/></button>
+                <DatePicker onChange={this.Calendar}>
+                    </DatePicker>
+                    </div>
+                    </React.Fragment>
+                    :null}
             </div>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
-  );
-};
+            <div className='footer'>
+                <div id="container">
+                    <div id="cont">
+                        <div className="footer_center">
+                            <section id="icons">
+                                <button className="button-noborder">
+                                    <img src={themes} width="60" height="60" alt="Theme"/>
+                                </button>
+                                <button className="button-noborder" onClick={this.openCalendar}>
+                                    <img src={calendar} width="60" height="60" alt="Calendar"/>
+                                </button>  
+                                <button className="button-noborder">
+                                    <img src={timer} width="60" height="60" alt="Timer"/>
+                                </button>
+                                <button className="button-noborder">
+                                    <img src={spotify} width="60" height="60" alt="Spotify"/>
+                                </button>
+                                <button className="button-noborder">
+                                    <img src={todolist} width="60" height="60" alt="To-do List"/> 
+                                </button>
+                                <button className="button-noborder">
+                                    <img src={sounds} width="60" height="60" alt="Sounds"/>
+                                </button>    
+                                <button className="button-noborder">
+                                    <img src={stickers} width="60" height="60" alt="Sticky"/>
+                                </button>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </React.Fragment>
+        );
+        
+    }
+}
+
 export default Dock;
