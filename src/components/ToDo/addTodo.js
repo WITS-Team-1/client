5bca946f741f611241ca46baa57b8aeccf8c44a8
asheik55 <asheik55@uwo.ca>
2022-03-25 12:53:09 -0400

    version 1

---
+++ src/components/ToDo/addTodo.js
@@ -0,0 +1,42 @@
+import React, { useState, useRef } from 'react';
+import './ToDoItem/Todo.css';
+import taskButton from '../assets/images/ToDo/coolicon.png';
+
+
+
+let counter = 0;
+function AddTodo(props) {
+  const [todo, setTodo] = useState('');
+  const inputRef = useRef();
+
+  const updateInput = (e) => {
+    setTodo(e.target.value);
+  };
+  const submitTodo = (e) => {
+    e.preventDefault();
+    if (counter < 4) {
+      props.addTodoFn(todo);
+      inputRef.current.value = '';
+      counter++;
+    }
+  };
+
+  return (
+    <div className='addTodoContainer'>
+      <form onSubmit={submitTodo}>
+        <button type='submit'>
+          <img src={taskButton} />
+        </button>
+        <input
+          ref={inputRef}
+          id='addTodoInput'
+          onChange={updateInput}
+          type='text'
+          placeholder='Add new task'
+        ></input>
+      </form>
+    </div>
+  );
+}
+
+export default AddTodo;
